fcdb421eadc67691d285ebad03e6e3f0a3bfaadf
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-11 11:00:21 +0530

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel MYProduct
                 </div>
 
                 <div class="links">
