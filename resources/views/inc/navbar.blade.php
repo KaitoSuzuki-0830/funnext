c05c72f12b80df07bf009c5f4e50359372eef4cd
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-12 14:24:15 +0530

    profile

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -9,12 +9,12 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav ">
 
             </ul>
 
             <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav ">
                 <!-- Authentication Links -->
                 @guest
                     <li class="nav-item">
