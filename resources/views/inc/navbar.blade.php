8db74d61ab852c9f96ef1d6ca9d024faf95b8f48
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-12 14:32:50 +0530

    profile

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -9,21 +9,22 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">新規グループ投稿</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">000とは</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">ヘルプ</a>
-                </li>
-            </ul>
+
 
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
                 <!-- Authentication Links -->
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">新規グループ投稿</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">000とは</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">ヘルプ</a>
+                    </li>
+                </ul>
                 @guest
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
