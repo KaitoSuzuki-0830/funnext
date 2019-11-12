f5db65bb616ca17999ea7282a9d699780b4ec1cf
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-12 14:40:49 +0530

    profile

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -10,7 +10,6 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
 
-
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
                 <!-- Authentication Links -->
@@ -30,7 +29,8 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            <a class="dropdown-item" href="{{ route('user.profile') }}">My Profile</a>
+                            <a class="dropdown-item" href="{{ route('logout')}}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
