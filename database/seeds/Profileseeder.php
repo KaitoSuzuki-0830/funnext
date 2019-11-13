9bc6b3db4d810e87d044c1f92226f12b2d754b85
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-13 10:05:00 +0530

    profile

---
+++ database/seeds/Profileseeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfilesController;
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Profile;
+
+class Profileseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user=User::create([
+            'name'=>'kaito',
+            'email'=>'kaito@gmail.com',
+            'password'=>bcrypt('password')
+        ]);
+
+        Profile::create([
+            'user-id'=>$user->id,
+            'avatar' =>'uploads/avatar/sample.png',
+            'about' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. At architecto repellat sapiente nemo cupiditate eaque adipisci praesentium laboriosam, commodi assumenda sit quae minus impedit tempora sint amet. Eius, inventore dolore?',
+            'facebook' => 'https://www.facebook.com/kati-frantz',
+            'instagram'=>'https://www.instagram.com/kati',
+            'twitter'=>'https://www.twitter.com/kati'
+        ]);
+    }
+}
