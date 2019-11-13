22a16b3b59acd272d4073e50a0bd69219c089f82
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-13 16:10:26 +0530

    profile

--- database/seeds/Profileseeder.php
+++ database/seeds/Profileseeder.php
@@ -16,7 +16,7 @@ class Profileseeder extends Seeder
     {
         $user=User::create([
             'name'=>'kaito',
-            'email'=>'exple@gmail.com',
+            'email'=>'exmple@gmail.com',
             'password'=>bcrypt('password')
         ]);
 
