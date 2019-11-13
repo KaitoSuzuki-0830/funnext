d5baed545995833696fb65918b8abf422f895e69
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-13 15:32:06 +0530

    profile

--- app/group.php
+++ app/group.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class group extends Model
 {
-    //
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
