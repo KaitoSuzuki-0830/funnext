20d344c524719bf72a33cdbdbcb97befc4ede108
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-14 16:46:19 +0530

    　group

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -4,10 +4,10 @@
  <div class="card-header">グループ詳細</div>
  <div class="card-body">
      <h5 class="card-title">{{ $group->name}}</h5>
-     <img src="{{ secure_asset('uploads/groups/'.$post->featured)}}" alt="{{ $post->title }}" height="250px" width="100%">
-     <p class="card-text">{{ $post->description}}</p>
+     <img src="{{ secure_asset('uploads/groups/'.$group->featured)}}" alt="{{ $group->title }}" height="250px" width="100%">
+     <p class="card-text">{{ $group->description}}</p>
      <div class="float-left">
-         <a href="{{ route('groups.edit',['id'=>$post->id])}}" class="btn btn-info"><i class="far fa-edit"></i></a>
+         <a href="{{ route('groups.edit',['id'=>$group->id])}}" class="btn btn-info"><i class="far fa-edit"></i></a>
      </div>
  </div>
 </div>
