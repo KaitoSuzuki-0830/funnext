a67a8d2605160144f98d6589c74ddfa4cadd26df
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-14 10:09:53 +0530

    profile

---
+++ database/seeds/UserTableseeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Profile;
+
+class UserTableseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user=factory(App\User::class,1)->create();
+
+        Profile::create([
+            'user_id'=>$user->id,
+            'avatar' =>'uploads/avatar/sample.png',
+            'place'=>'Tokyo',
+            'about' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. At architecto repellat sapiente nemo cupiditate eaque adipisci praesentium laboriosam, commodi assumenda sit quae minus impedit tempora sint amet. Eius, inventore dolore?',
+            'facebook' => 'https://www.facebook.com/kati-frantz',
+            'instagram'=>'https://www.instagram.com/kati',
+            'twitter'=>'https://www.twitter.com/kati'
+        ]);
+    }
+}
