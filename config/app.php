192f4837ad14896f81a65e485747fd00d2defe83
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-14 19:53:48 +0530

    category

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Relaccumulation'),
+    'name' => env('APP_NAME', 'Relaccum'),
 
     /*
     |--------------------------------------------------------------------------
