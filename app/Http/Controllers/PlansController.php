9a9ee2da0ddeecf5a44c6b68b0a613cc06dec233
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-15 16:16:12 +0530

    　plan

--- app/Http/Controllers/PlansController.php
+++ app/Http/Controllers/PlansController.php
@@ -68,7 +68,7 @@ class PlansController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(plan $plan)
     {
         return view('plans.show')->with('plan',$plan)
                                 ->with('user',Auth::user());
