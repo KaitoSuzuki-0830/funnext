909ce1310d50d0d525705110e34304919323fe00
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-20 10:06:12 +0530

    　pref

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -33,7 +33,7 @@
 <div class="form-group">
     <label for="place">場所</label>
     @foreach ($prefecture as $pref)
-    <select name="prefecture" id="pref_id">{{$user->profile->prefecture}}</select>
+    <select name="prefecture" id="pref_id">{{$pref}}</select>
     @endforeach
 </div>
 <div class="form-group">
