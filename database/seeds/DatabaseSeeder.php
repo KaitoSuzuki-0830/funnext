ccc2de075a25940cdeb181e4eb8766947c00670c
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-20 12:00:56 +0530

    　profile

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //$this->call(UserTableseeder::class);
-        $this->call(UserTableSeeder::class);
+        $this->call(UserTableseeder::class);
 
     }
 }
