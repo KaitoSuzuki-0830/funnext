eb1ae655fd2534641595af7a34ecbfe862f68696
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 16:42:11 +0530

    　groups

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -13,11 +13,11 @@
   <h3>グループ一覧</h3>
   @foreach($groups as $group)
   <div class="card bg-dark text-white">
-    <img src="{{asset($groups->featured)}}" class="card-img" alt="{{$groups->title}}" height="300px" width="33%">
+    <img src="{{asset($group->featured)}}" class="card-img" alt="{{$group->title}}" height="300px" width="33%">
     <div class="card-img-overlay">
-        <h5 class="card-title">{{$groups->name}}</h5>
+        <h5 class="card-title">{{$group->name}}</h5>
         <p class="card-text">members</p>
-        <p class="card-text"><i class="fas fa-tag"></i> {{$categories->name}}</p>
+        <p class="card-text"><i class="fas fa-tag"></i> {{$group->category->name}}</p>
     </div>
   @endforeach
   </div>
