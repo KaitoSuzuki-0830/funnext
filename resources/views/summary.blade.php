e4a25c395a8a8eb17a381d9e21b1012fa094efef
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 15:19:53 +0530

    　summary

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -5,9 +5,9 @@
     </div>
     <div>グループ一覧</div>
     <div class="card bg-dark text-white">
-        <img src="{{ $group->featured}}" class="card-img" alt="featured">
+        <img src="{{ $groups->featured}}" class="card-img" alt="featured">
         <div class="card-img-overlay">
-            <h5 class="card-title">{{ $group->name}}</h5>
+            <h5 class="card-title">{{ $groups->name}}</h5>
             <p class="card-text">members</p>
             <p class="card-text"><i class="fas fa-tag"></i> {{$categories->name}}</p>
         </div>
