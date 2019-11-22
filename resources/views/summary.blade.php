4a7a00131d4f5b004ca040c1e36bf253f7b0ccf9
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 15:04:50 +0530

    　summary

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -3,4 +3,13 @@
     <div class="container-fluid">
         <img src="" alt="image">
     </div>
+    <div>グループ一覧</div>
+    <div class="card bg-dark text-white">
+        <img src="{{ $group->featured}}" class="card-img" alt="featured">
+        <div class="card-img-overlay">
+            <h5 class="card-title">{{ $group->name}}</h5>
+            <p class="card-text">members</p>
+            <p class="card-text"><i class="fas fa-tag"></i> {{$categories->name}}</p>
+        </div>
+    </div>
 @endsection
