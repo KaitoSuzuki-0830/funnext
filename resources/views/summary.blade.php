0a2111455c33a99209619d403413073a34220af8
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 15:30:33 +0530

    　summary

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -3,7 +3,9 @@
     <div class="container-fluid">
         <img src="" alt="image">
     </div>
+    <div class="card-body">
     <div>グループ一覧</div>
+    @foreach($groups as $group)
     <div class="card bg-dark text-white">
         <img src="{{ $groups->featured}}" class="card-img" alt="featured">
         <div class="card-img-overlay">
@@ -11,5 +13,8 @@
             <p class="card-text">members</p>
             <p class="card-text"><i class="fas fa-tag"></i> {{$categories->name}}</p>
         </div>
+    @endforeach
     </div>
+    </div>
+
 @endsection
