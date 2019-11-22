07ed3d09081d2c42d769bdbd031f4ee6e8e78ca2
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 11:42:06 +0530

    　plan

--- resources/views/plans/create.blade.php
+++ resources/views/plans/create.blade.php
@@ -30,18 +30,18 @@
     </div>
     <div class="form-group">
         <div class="row">
-            <div class="col-sm-2"><label for="name">場所1</label></div>
-            <div class="col-sm-4">
+            <div class="col-sm-1"><label for="name">場所1</label></div>
+            <div class="col-sm-5">
                 <select name="prefecture" id="pref_id">
                 @foreach ($prefectures as $pref)
                 <option value="{{$pref->name}}">{{$pref->name}}</option>
                 @endforeach
                 </select>
             </div>
-            <div class="col-sm-2">
+            <div class="col-sm-1">
                 <label for="place">場所2</label>
             </div>
-            <div class="col-sm-4">
+            <div class="col-sm-5">
                 <input type="text" name="place" class="form-control" placeholder="具体的な住所を貼り付けてください">
             </div>
         </div>
