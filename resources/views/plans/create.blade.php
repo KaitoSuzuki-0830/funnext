4b682b0e72d537414be604f9ec4bf151baf23825
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 12:25:15 +0530

    　plan

--- resources/views/plans/create.blade.php
+++ resources/views/plans/create.blade.php
@@ -25,11 +25,14 @@
      <textarea name="description" rows="10" cols="10" class="form-control"></textarea>
     </div>
     <div class="form-group">
-        <label for="price">値段（１人当り)</label>
-        <input type="text" name="price" class="form-control">
+        <div class="row">
+            <div class="col-sm-2"><label for="price">参加費（１人当り)</label></div>
+            <div class="col-sm-10"><input type="text" name="price" class="form-control"></div>
+        </div>
     </div>
     <div class="form-group">
         <div class="row">
+            <div class="col-sm-1"></div>
             <div class="col-sm-1"><label for="name">場所1:</label></div>
             <div class="col-sm-1">
                 <select name="prefecture" id="pref_id">
@@ -40,7 +43,7 @@
             </div>
             <div class="col-sm-1"></div>
             <div class="col-sm-1"><label for="place">場所2:</label></div>
-            <div class="col-sm-8">
+            <div class="col-sm-7">
                 <input type="text" name="place" class="form-control" placeholder="具体的な住所を貼り付けてください">
             </div>
         </div>
