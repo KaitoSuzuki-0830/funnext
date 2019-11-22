a7c61cd17838c3fb0509604973abd71652852197
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 18:03:39 +0530

    　groups

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -2,11 +2,13 @@
 @section('content')
 <div class="input-group bg-dark text-white p-3">
     <div class="col-md-2"></div>
-    <div class="col-md-4 text-left">
+    <div class="col-md-4">
         <input type="text"  placeholder="グループ検索">
     </div>
-    <div class="col-md-4 text-right">
+    <div class="col-md-2">
         <button type="submit" class="btn btn-success">グループ</button>
+    </div>
+    <div class="col-md-2">
         <button type="submit" class="btn btn-danger">イベント一覧</button>
     </div>
     <div class="col-md-2"></div>
