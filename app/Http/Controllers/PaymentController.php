b48eca4344bfcf8c19663bbfc113001db8a0f95b
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 10:28:28 +0530

    　plan

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,7 +9,7 @@ use Stripe\Charge;
 class PaymentController extends Controller
 {
     public function pay(Request $request){
-        Stripe::setApiKey(env('STRIPE_KEY'));
+        Stripe::setApiKey('sk_test_UEqp4NMzfktQXsr4Tvp6yrR400oKsFFHIR');
 
         $charge = Charge::create(array(
             'amount'=>100,
