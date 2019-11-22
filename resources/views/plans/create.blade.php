27da998eb160cf249c3c9ff7939fa354af849bcf
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 11:48:32 +0530

    　plan

--- resources/views/plans/create.blade.php
+++ resources/views/plans/create.blade.php
@@ -31,7 +31,7 @@
     <div class="form-group">
         <div class="row">
             <div class="col-sm-1"><label for="name">場所1:</label></div>
-            <div class="col-sm-2">
+            <div class="col-sm-1">
                 <select name="prefecture" id="pref_id">
                 @foreach ($prefectures as $pref)
                 <option value="{{$pref->name}}">{{$pref->name}}</option>
