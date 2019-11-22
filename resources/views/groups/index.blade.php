41ecf33be661dad24d1ff826a6a077ed8c45d131
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 16:28:04 +0530

    　groups

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -1,19 +1,25 @@
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid">
-    <img src="" alt="image">
+    <img src="find.jpg" alt="image">
 </div>
-    <div class="card-body">
-    <h3>グループ一覧</h3>
-    @foreach($groups as $group)
-    <div class="card bg-dark text-white">
-        <img src="{{asset($groups->featured)}}" class="card-img" alt="{{$groups->title}}" height="300px" width="33%">
-        <div class="card-img-overlay">
-            <h5 class="card-title">{{$groups->name}}</h5>
-            <p class="card-text">members</p>
-            <p class="card-text"><i class="fas fa-tag"></i> {{$categories->name}}</p>
-        </div>
-    @endforeach
+<div class="input-group">
+    <input type="text" class="form-control" placeholder="グループ検索">
+    <span class="input-group-btn">
+        <button type="button" class="btn btn-default">ボタン</button>
+    </span>
+</div>
+<div class="card-body">
+  <h3>グループ一覧</h3>
+  @foreach($groups as $group)
+  <div class="card bg-dark text-white">
+    <img src="{{asset($groups->featured)}}" class="card-img" alt="{{$groups->title}}" height="300px" width="33%">
+    <div class="card-img-overlay">
+        <h5 class="card-title">{{$groups->name}}</h5>
+        <p class="card-text">members</p>
+        <p class="card-text"><i class="fas fa-tag"></i> {{$categories->name}}</p>
     </div>
-    </div>
+  @endforeach
+  </div>
+</div>
 @endsection
