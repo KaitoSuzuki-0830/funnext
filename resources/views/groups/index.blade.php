4475d526ccf50b41e5dad0395e3917bd32ec60e4
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 16:57:04 +0530

    　groups

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<div class="container-fluid">
-    <img src="find.jpg" alt="image">
-</div>
 <div class="input-group bg-dark text-white p-3">
     <input type="text" class="form-control" placeholder="グループ検索">
     <span class="input-group-btn">
