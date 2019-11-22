1bc6c71a41250b0ebe6d97b09330a340a6faaf0a
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-22 10:48:54 +0530

    　plan

--- resources/views/plans/index.blade.php
+++ resources/views/plans/index.blade.php
@@ -14,9 +14,9 @@
         <tr>
            <td> <img src="{{ asset('uploads/plan/'.$plan->featured)}}" alt="{{ $plan->title }}" height="90px" width="90px"style="border-radius: 80%"></td>
            <td>{{ $plan->title}}</td>
-           <td><a href="{{ route('plan.show',['id'=>$plan->id])}}" class="btn btn-primary"><i class="far fa-eye"></i></a></td>
+           <td><a href="{{ route('plans.show',['id'=>$plan->id])}}" class="btn btn-primary"><i class="far fa-eye"></i></a></td>
            <td>
-             <form action="{{ route('plan.destroy',['id' => $plan->id])}}" method="plan">
+             <form action="{{ route('plans.destroy',['id' => $plan->id])}}" method="plan">
                @csrf
                @method('DELETE')
                <button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
