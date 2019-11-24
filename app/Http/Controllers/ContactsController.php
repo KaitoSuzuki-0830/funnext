e751c6b321205a2069c5308a3c987fed0d66eff0
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-24 15:14:44 +0530

    　contacts

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactRequest;
 use App\Http\Controllers\Controller;
-use App\Contact;
+use App\Contacts;
 
 class ContactsController extends Controller
 {
