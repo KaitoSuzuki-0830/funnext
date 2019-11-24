8b09c99e53f71163b3e87f137b74b2447bfa9f7b
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-25 02:28:34 +0530

    　socialite

--- app/User.php
+++ app/User.php
@@ -54,4 +54,8 @@ class User extends Authenticatable
         return $this->hasMany('App\SocialAccount');
     }
 
+    public function identities(){
+        return $this->hasMany('App\SocialIdentity');
+    }
+
 }
