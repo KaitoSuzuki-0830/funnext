26c8421a9d2fb737aeaccd27798335579f0be4b5
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-25 23:02:59 +0530

    　plan

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -16,7 +16,7 @@
 
     @foreach($groups as $group)
         <tr>
-           <td><img src="{{ $group->featured }}" alt="{{ $group->name }}" height="80px" width="50px;"></td>
+           <td><img src="{{ $group->featured }}" alt="{{ $group->name }}" height="80px" width="100px;"></td>
            <td>{{ $group->name}}</td>
            <td><a href="{{ route('groups.show',['id'=>$group->id])}}" class="btn btn-primary"><i class="far fa-eye"></i></a></td>
            <td>
