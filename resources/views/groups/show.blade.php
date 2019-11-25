16457046b388a977be6ec323e03db8fca94abb2a
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-25 12:31:01 +0530

    　single

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -45,41 +45,40 @@
             <p><i class="fas fa-users"></i></p>
         </div>
     </div>
-    <hr>
+<hr>
     <div class="row">
         <div class="col-sm-6">
             <h5>イベントリスト (Count)</h5>
             @foreach($plans as $plan)
-            <div class="card-deck" style="width:100%;">
-            <img src="{{ $plan->featured }}" class="card-img-top" alt="{{$plan->name}}">
-            <div class="card-body">
-            <div class="card-title"><h3>{{$plan->title}}</h3></div>
-            {{-- <p class="card-text"><i class="fas fa-map-marker-alt"></i> {{$plan->prefecture->name}}</p> --}}
-            <p class="card-text"><i class="far fa-map"></i> {{$plan->place}}</p>
-            <p class="card-text"><i class="fas fa-tag"></i>{{$group->category->name}}</p>
-            <p class="card-text">{{$plan->description}}</p>
-            <p class="card-text inline"><i class="fas fa-yen-sign"></i> {{$plan->price}}
-                <form action="{{ route('pay') }}" method="POST">
-                        {{ csrf_field() }}
-                     <script
-                         src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                         data-key="pk_test_EtHmYNmqSKAqc3JTRo7H98vx003VgTohui"
-                         data-amount="{{$plan->price}}"
-                         data-name="イベント参加"
-                         data-label="お支払い"
-                         data-description="Laravel-Myproduct.payment"
-                         data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-                         data-locale="auto"
-                         data-currency="JPY">
-                     </script>
-                </form>
-            </p>
-    </div>
-    </a>
-    </div>
-    </div>
-@endforeach
+                <div class="card-deck" style="width:100%;">
+                    <img src="{{ $plan->featured }}" class="card-img-top" alt="{{$plan->name}}" height="200px" width="200px">
+                        <div class="card-body">
+                            <div class="card-title"><h3>{{$plan->title}}</h3></div>
+                            {{-- <p class="card-text"><i class="fas fa-map-marker-alt"></i> {{$plan->prefecture->name}}</p> --}}
+                            <p class="card-text"><i class="far fa-map"></i> {{$plan->place}}</p>
+                            <p class="card-text"><i class="fas fa-tag"></i>{{$group->category->name}}</p>
+                            <p class="card-text">{{$plan->description}}</p>
+                            <p class="card-text" style="inline"><i class="fas fa-yen-sign"></i> {{$plan->price}}
+                                <form action="{{ route('pay') }}" method="POST">
+                                    {{ csrf_field() }}
+                                <script
+                                    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                                    data-key="pk_test_EtHmYNmqSKAqc3JTRo7H98vx003VgTohui"
+                                    data-amount="{{$plan->price}}"
+                                    data-name="イベント参加"
+                                    data-label="お支払い"
+                                    data-description="Laravel-Myproduct.payment"
+                                    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+                                    data-locale="auto"
+                                    data-currency="JPY">
+                                </script>
+                                </form>
+                            </p>
+                        </div>
+                </div>
         </div>
+        @endforeach
+    </div>
         <div class="col-sm-6">
             <a href="#">一覧</a>
         </div>
