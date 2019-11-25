aa0870becc82cd76177c4cf611e699f319c934b1
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-26 00:25:53 +0530

    　plan

--- config/services.php
+++ config/services.php
@@ -46,4 +46,10 @@ return [
         'redirect' => env('TWITTER_URL'),
     ],
 
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'), // Your GitHub Client ID
+        'client_secret' => env('GITHUB_CLIENT_SECRET'), // Your GitHub Client Secret
+        'redirect' => env('GITHUB_URL'),
+    ],
+
 ];
