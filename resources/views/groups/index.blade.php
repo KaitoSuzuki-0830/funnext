df012e814b1b209db9dca5404b1ac64938fb0d6d
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-25 23:09:06 +0530

    　plan

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -17,9 +17,9 @@
     @foreach($groups as $group)
         <tr>
            <td　width="100"><img src="{{ $group->featured }}" alt="{{ $group->name }}" height="80px" width="100px;"></td>
-           <td>{{ $group->name}}</td>
-           <td><a href="{{ route('groups.show',['id'=>$group->id])}}" class="btn btn-primary"><i class="far fa-eye"></i></a></td>
-           <td>
+           <td width="100">{{ $group->name}}</td>
+           <td width="100"><a href="{{ route('groups.show',['id'=>$group->id])}}" class="btn btn-primary"><i class="far fa-eye"></i></a></td>
+           <td width="100">
              <form action="{{ route('groups.destroy',['id' => $group->id])}}" method="groups">
                @csrf
                @method('DELETE')
