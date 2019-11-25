68ea194ef76826322e90c90c598b356d00eab20b
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-25 14:59:12 +0530

    　single

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -61,7 +61,7 @@
                                 <form action="{{ route('pay') }}" method="POST">
                                     {{ csrf_field() }}
                                     <script
-                                        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                                        src="https://checkout.stripe.com/checkout.js" class="stripe-button justify-content-center"
                                         data-key="pk_test_EtHmYNmqSKAqc3JTRo7H98vx003VgTohui"
                                         data-amount="{{$plan->price}}"
                                         data-name="参加費を支払う"
