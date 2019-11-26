25d5e372c07beca584689b8e116271c493179683
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-26 11:45:00 +0530

    　group

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -76,6 +76,7 @@
                                     <a href="{{ url('/login/github') }}" class="btn btn-github"><i class="fab fa-github"></i> Github</a>
                                     <a href="{{ url('/login/twitter') }}" class="btn btn-twitter"><i class="fab fa-twitter"></i> Twitter</a>
                                     <a href="{{ url('/login/facebook') }}" class="btn btn-facebook"><i class="fab fa-facebook-square"></i> Facebook</a>
+                                    <a href="{{ url('/login/google')}}" class="brn brn-google"><i class="fab fa-google"></i> Google</a>
                                 </div>
                                 <div class="col-md-3"></div>
                             </div>
