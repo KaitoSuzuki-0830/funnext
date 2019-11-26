28f7af6e2ed74582acdc0156f5f7c5d724b317a6
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-26 12:16:33 +0530

    　group

--- resources/views/plans/show.blade.php
+++ resources/views/plans/show.blade.php
@@ -1,16 +1,33 @@
 @extends('layouts.app')
 @section('content')
-
-<div class="card">
- <div class="card-header">plan Details</div>
- <div class="card-body">
-    <h5 class="card-title">{{ $plan->title}}</h5>
-    <img src="{{ asset('uploads/plans/'.$plan->featured)}}" alt="{{ $plan->title }}" height="250px" width="100%">
-    <p class="card-text">{{ $plan->description}}</p>
-    <div class="float-left">
-        <a href="{{ route('plans.edit',['id'=>$plan->id])}}" class="btn btn-info"><i class="far fa-edit"></i></a>
+ <div class="card-header">イベント　詳細　</div>
+ <div class="card" style="width:100%;">
+        <img src="{{ $plan->featured }}" class="card-img-top" alt="{{$plan->title}}" height="200px" width="200px">
+            <div class="card-body">
+                <div class="card-title"><h3>{{$plan->title}}</h3></div>
+                {{-- <p class="card-text"><i class="fas fa-map-marker-alt"></i> {{$plan->prefecture->name}}</p> --}}
+                <p class="card-text"><i class="far fa-map"></i> {{$plan->place}}</p>
+                <p class="card-text"><i class="fas fa-tag"></i> {{$group->category->name}}</p>
+                <p class="card-text">{{$plan->description}}</p>
+                <p class="card-text"><i class="fas fa-yen-sign"></i> {{$plan->price}}
+                    <form action="{{ route('pay') }}" method="POST">
+                        {{ csrf_field() }}
+                        <script
+                            src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                            data-key="pk_test_EtHmYNmqSKAqc3JTRo7H98vx003VgTohui"
+                            data-amount="{{$plan->price}}"
+                            data-name="参加費を支払う"
+                            data-label="参加する"
+                            data-description="Laravel-Myproduct.payment"
+                            data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+                            data-locale="auto"
+                            data-currency="JPY">
+                        </script>
+                    </form>
+                </p>
+            </div>
     </div>
-  </div>
+
  </div>
 
 @endsection
