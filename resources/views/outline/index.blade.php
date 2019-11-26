91e7b51f1becb8ca15c24d2c961c8b30f52f3a1f
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-27 01:44:18 +0530

    　outline

--- resources/views/outline/index.blade.php
+++ resources/views/outline/index.blade.php
@@ -28,7 +28,7 @@
   <div class="d-inline-block">
   <div class="card-deck" style="width:24rem;">
     <a href="{{route('groups.show',['id'=>$group->id])}}" class="card">
-    <img src="{{asset($group->featured)}}" class="card-img" alt="{{$group->name}}" height="200px" width="250px">
+    <img src="{{asset($group->featured)}}" class="card-img" alt="{{$group->name}}" height="200px" width="300px">
     <div class="card-img-overlay">
         <div class="card-title"><h3>{{$group->name}}</h3></div>
         <p class="card-text"><i class="fas fa-users"></i> members</p>
