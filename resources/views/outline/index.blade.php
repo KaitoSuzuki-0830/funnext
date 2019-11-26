cf5a2e1cc373828cfdb7669562118043c49a4cd1
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-26 19:16:37 +0530

    　table

--- resources/views/outline/index.blade.php
+++ resources/views/outline/index.blade.php
@@ -31,7 +31,7 @@
         <div class="card-title"><h3>{{$group->name}}</h3></div>
         <p class="card-text"><i class="fas fa-users"></i> members</p>
         <p class="card-text"><i class="fas fa-tag"></i>{{$group->category->name}}</p>
-        <p class="card-text"><i class="fas fa-plus-circle fa-2x"></i></p>
+        <a class="card-text"><i class="fas fa-plus-circle fa-2x"></i></a>
     </div>
     </a>
     </div>
