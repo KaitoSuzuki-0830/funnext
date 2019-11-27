12c13c58183b79c69bda98920209c0c37a076ec7
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-27 11:51:40 +0530

    　group

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,37 @@
+<footer>
+        <div class="container-fluid">
+            <div class="input-group bg-dark p-5">
+                <a href="/about">使い方</a>
+            <hr>
+            <tbody class="row">
+                <tbody class="col-md-8">
+                    <thead>
+                        <th>アカウント</th>
+                        <th>探す・作る</th>
+                        <th>サービス</th>
+                    </thead>
+                    <tbody>
+                        <td><a href="">Sign up</a></td>
+                        <td><a href="">グループ</a></td>
+                        <td><a href="">About</a></td>
+                        <td><a href="">Log in</a></td>
+                        <td><a href="">イベント</a></td>
+                        <td><a href="">Contact</a></td>
+                        <td><a href=""></a></td>
+                        <td><a href="">カテゴリー</a></td>
+                        <td><a href=""></a></td>
+                        </tbody>
+                </tbody>
+                <div class="col-md-4">
+                    <thead>
+                        <th>Follow us</th>
+                    </thead>
+                </div>
+            </tbody>
+
+            <hr>
+                <small>©︎ Relaccum 2019 ・　KaitoSuzuki</small>
+                <p>Terms of Service ・ Privacy Policy ・ Cookie Policy</p>
+            </div>
+        </div>
+</footer>
--- resources/views/outline/index.blade.php
+++ resources/views/outline/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<header>
 <div class="input-group bg-dark text-white p-2">
     {{-- Search box  --}}
     <div class="col-md-1"></div>
@@ -16,9 +15,6 @@
     </div>
     <div class="col-md-1"></div>
 </div>
-</header>
-
-<main>
 <div class="row">
     <div class="col-md-10"><h3>グループ</h3></div>
     <div class="col-md-2"><a href="{{route('groups.index')}}"><h5>See all</h5></a></div>
@@ -59,43 +55,4 @@
         </div>
     </div>
 @endforeach
-</main>
-
-<footer>
-    <div class="container-fluid">
-        <div class="input-group bg-dark p-5">
-            <a href="/about">使い方</a>
-        <hr>
-        <tbody class="row">
-            <tbody class="col-md-8">
-                <thead>
-                    <th>アカウント</th>
-                    <th>探す・作る</th>
-                    <th>サービス</th>
-                </thead>
-                <tbody>
-                    <td><a href="">Sign up</a></td>
-                    <td><a href="">グループ</a></td>
-                    <td><a href="">About</a></td>
-                    <td><a href="">Log in</a></td>
-                    <td><a href="">イベント</a></td>
-                    <td><a href="">Contact</a></td>
-                    <td><a href=""></a></td>
-                    <td><a href="">カテゴリー</a></td>
-                    <td><a href=""></a></td>
-                    </tbody>
-            </tbody>
-            <div class="col-md-4">
-                <thead>
-                    <th>Follow us</th>
-                </thead>
-            </div>
-        </tbody>
-
-        <hr>
-            <small>©︎ Relaccum 2019 ・　KaitoSuzuki</small>
-            <p>Terms of Service ・ Privacy Policy ・ Cookie Policy</p>
-        </div>
-    </div>
-</footer>
 @endsection
