60d0f017cc06439f1b051033874eb55e1ff1bb75
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-27 16:59:57 +0530

    　category

--- app/Http/Controllers/OutlineController.php
+++ app/Http/Controllers/OutlineController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\group;
 use App\Category;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 
 class OutlineController extends Controller
