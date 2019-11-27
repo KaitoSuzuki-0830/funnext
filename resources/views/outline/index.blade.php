22b975357faaab447bcdb8bde7f67473038c924e
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-27 10:57:38 +0530

    　group

--- resources/views/outline/index.blade.php
+++ resources/views/outline/index.blade.php
@@ -33,7 +33,7 @@
         <div class="card-title"><h3>{{$group->name}}</h3></div>
         <p class="card-text"><i class="fas fa-users"></i> members</p>
         <p class="card-text"><i class="fas fa-tag"></i>{{$group->category->name}}</p>
-        <a href="{{ route('group.join',['group_id'=>$group->id,'id'=>Auth::id()])}}" class="card-text float-right"><i class="fas fa-plus-circle fa-2x"></i></a>
+        <p><a href="{{ route('group.join',['group_id'=>$group->id,'id'=>Auth::id()])}}" class="card-text"><i class="fas fa-plus-circle fa-2x"></i></a></p>
     </div>
     </a>
     </div>
