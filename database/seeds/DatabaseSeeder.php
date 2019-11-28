6a3495165afc3bf46969e92d8c98ccde296878ec
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-28 11:08:05 +0530

    　single

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         //$this->call(UserTableseeder::class);
         $this->call(UserTableseeder::class);
+        $this->call(PrefSeeder::class);
+
 
     }
 }
