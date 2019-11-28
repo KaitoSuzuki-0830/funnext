aaf3ce042996924287be1824a51cf1493d538c9f
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-28 17:28:38 +0530

    　seeder

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -7,13 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(group::class, function (Faker $faker) {
     return [
-        $name = $faker->sentence(3),
-        'name' => $name,
-        'slug' => str_slug($name),
+        'name' => $faker->name,
+        'slug' => $faker->sentence(3),
         'description' => $faker->paragraph(3),
         'featured' => secure_asset('uploads/groups/php2.png'),
-        'category_id' => $faker->randomDigitNotNull,
-        'user_id' => $faker->randomDigitNotNull
-
+        'category_id' => $faker->randomDigit,
+        'user_id' => $faker->randomDigit
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
         //$this->call(UserTableseeder::class);
         //$this->call(UserTableseeder::class);
         //$this->call(PrefSeeder::class);
-        //$this->call(GroupsTableSeeder::class);
-        $this->call(PlansTableSeeder::class);
+        $this->call(GroupsTableSeeder::class);
+        //$this->call(PlansTableSeeder::class);
 
 
     }
