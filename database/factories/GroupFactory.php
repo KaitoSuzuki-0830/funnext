f1159a583d0cfd18ce45ae208475378aa4bb9091
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-28 16:42:26 +0530

    　single

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -7,14 +7,13 @@ use Faker\Generator as Faker;
 
 $factory->define(group::class, function (Faker $faker) {
     return [
-        'id'=>$faker->randomDigitNot(0),
-        $name=$faker->sentence(3),
-        'name'=> $name,
+        $name = $faker->sentence(3),
+        'name' => $name,
         'slug' => str_slug($name),
-        'description' =>$faker->paragraph(3),
+        'description' => $faker->paragraph(3),
         'featured' => secure_asset('uploads/groups/php2.png'),
-        'category_id' =>$faker->randomDigit,
-        'user_id'=>$faker->randomDigitNotNull
+        'category_id' => $faker->randomDigit,
+        'user_id' => $faker->randomDigitNotNull
 
     ];
 });
