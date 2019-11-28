d4795a9f42ebcece68ca8509e69e0f41c5f37eb9
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-28 21:48:45 +0530

    　contact

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,36 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('contact.confirm') }}">
-    @csrf
+<div class="card">
+    <div class="card-header">お問い合わせ</div>
+    <div class="card-body">
+            <form method="POST" action="{{ route('contact.confirm') }}">
+                    @csrf
 
-    <label>メールアドレス</label>
-    <input
-        name="email"
-        value="{{ old('email') }}"
-        type="text">
-    @if ($errors->has('email'))
-        <p class="error-message">{{ $errors->first('email') }}</p>
-    @endif
+                    <label>メールアドレス</label>
+                    <input
+                        name="email"
+                        value="{{ old('email') }}"
+                        type="text">
+                    @if ($errors->has('email'))
+                        <p class="error-message">{{ $errors->first('email') }}</p>
+                    @endif
 
-    <label>タイトル</label>
-    <input
-        name="title"
-        value="{{ old('title') }}"
-        type="text">
-    @if ($errors->has('title'))
-        <p class="error-message">{{ $errors->first('title') }}</p>
-    @endif
+                    <label>タイトル</label>
+                    <input
+                        name="title"
+                        value="{{ old('title') }}"
+                        type="text">
+                    @if ($errors->has('title'))
+                        <p class="error-message">{{ $errors->first('title') }}</p>
+                    @endif
 
 
-    <label>お問い合わせ内容</label>
-    <textarea name="body">{{ old('body') }}</textarea>
-    @if ($errors->has('body'))
-        <p class="error-message">{{ $errors->first('body') }}</p>
-    @endif
+                    <label>お問い合わせ内容</label>
+                    <textarea name="body">{{ old('body') }}</textarea>
+                    @if ($errors->has('body'))
+                        <p class="error-message">{{ $errors->first('body') }}</p>
+                    @endif
 
-    <button type="submit">
-        入力内容確認
-    </button>
-</form>
+                    <button type="submit">
+                        入力内容確認
+                    </button>
+            </form>
+    </div>
+</div>
+
 @endsection
