0745f9df107685d7b75fd893d5c9b80e6163b6cd
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-11-29 17:10:16 +0530

    　group

--- database/seeds/UserTableseeder.php
+++ database/seeds/UserTableseeder.php
@@ -19,5 +19,15 @@ class UserTableseeder extends Seeder
             'password'=>bcrypt('password')
         ]);;
 
+        Profile::create([
+            'user_id'=>$user->id,
+            'avatar' =>secure_asset('uploads/avatar/sample.png'),
+            'about' => '自己紹介を書くことができます',
+            'pref_id'=>1,
+            'facebook' => 'facebookアカウント追加してください',
+            'instagram'=>'その他webリンクを追加する',
+            'twitter'=>'Twitterアカウント追加してください'
+        ]);
+
     }
 }
