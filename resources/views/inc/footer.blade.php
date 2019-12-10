0e558f1e8bf4bdbaf51570b0e1ab814b149f2312
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-10 11:35:48 +0530

    　footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,24 +2,30 @@
     <div class="container-fluid">
         <div class="container">
             <div class="row">
-                <ul class="col-md-4" id="footerlist">
+                <div class="col-md-4">
+                    <ul id="footerlist">
                         <h3>ABOUT</h3>
                         <li class="footercontent">Relaccumとは</li>
                         <li class="footercontent">利用規約</li>
                         <li class="footercontent">お問い合わせ</li>
-                </ul>
-                <ul class="col-md-4" id="footerlist">
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul id="footerlist">
                         <h3>Menu</h3>
                         <li class="footercontent"><a href="">グループを探す</a></li>
                         <li class="footercontent"><a href="">イベントを探す</a></li>
                         <li class="footercontent"><a href="">カテゴリーを探す</a></li>
-                </ul>
-                <ul class="col-md-4" id="footerlist">
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul id="footerlist">
                         <h3>Follow us</h3>
                         <li id="snsicon" class="footercontent"><a href=""><i class="fab fa-facebook fa-2x"></i></a></li>
                         <li id="snsicon" class="footercontent"><a href=""><i class="fab fa-instagram fa-2x"></i></a></li>
                         <li id="snsicon" class="footercontent"><a href=""><i class="fab fa-twitter fa-2x"></i></a></li>
-                </ul>
+                    </ul>
+                </div>
             </div>
             <div class="row">
                 <div class="col-md-12">
