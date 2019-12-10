d74d512400c2a67ed6993642965ec4031c3a2847
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-10 11:05:06 +0530

    　footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,31 +2,29 @@
     <div class="container-fluid">
         <div class="container">
             <div class="row">
-                <tbody class="col-md-4">
-                    <thead>
-                        <th>アカウント</th>
-                        <th>探す・作る</th>
-                        <th>サービス</th>
-                    </thead>
-                </tbody>
-                <tbody class="col-md-4">
-                        <td><a href="">Sign up</a></td>
-                        <td><a href="">グループ</a></td>
-                        <td><a href="">About</a></td>
-                        <td><a href="">Log in</a></td>
-                        <td><a href="">イベント</a></td>
-                        <td><a href="">Contact</a></td>
-                        <td><a href=""></a></td>
-                        <td><a href="">カテゴリー</a></td>
-                        <td><a href=""></a></td>
-                </tbody>
-                    <tbody class="col-md-4">
-                        <td>Follow us</td>
-                    </tbody>
+                <ul class="col-md-4" style="none">
+                        <li>アカウント</li>
+                        <li>探す・作る</li>
+                        <li>サービス</li>
+                </ul>
+                <ul class="col-md-4" style="none">
+                        <li><a href="">Sign up</a></li>
+                        <li><a href="">グループ</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Log in</a></li>
+                        <li><a href="">イベント</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href=""></a></li>
+                        <li><a href="">カテゴリー</a></li>
+                        <li><a href=""></a></li>
+                </ul>
+                <ul class="col-md-4" style="none">
+                        <h3>Follow us</h3>
+                </ul>
             </div>
             <div class="col-md 12">
                 <small>©︎Relaccum 2019 ・　KaitoSuzuki</small>
-                <p>Terms of Service ・ Privacy Policy ・ Cookie Policy</p>
+                <h5>Terms of Service ・ Privacy Policy ・ Cookie Policy</h5>
             </div>
         </div>
     </div>
