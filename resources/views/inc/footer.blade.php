f96d4b8ed69a929151f8f1cce7502a846c2db9a7
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-10 11:12:37 +0530

    　footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,29 +2,26 @@
     <div class="container-fluid">
         <div class="container">
             <div class="row">
-                <ul class="col-md-4" style="none">
+                <ul class="col-md-4" id="footerlist">
                         <li>アカウント</li>
                         <li>探す・作る</li>
                         <li>サービス</li>
                 </ul>
-                <ul class="col-md-4" style="none">
+                <ul class="col-md-4" id="footerlist">
                         <li><a href="">Sign up</a></li>
                         <li><a href="">グループ</a></li>
                         <li><a href="">About</a></li>
                         <li><a href="">Log in</a></li>
                         <li><a href="">イベント</a></li>
                         <li><a href="">Contact</a></li>
-                        <li><a href=""></a></li>
                         <li><a href="">カテゴリー</a></li>
-                        <li><a href=""></a></li>
                 </ul>
-                <ul class="col-md-4" style="none">
+                <ul class="col-md-4" id="footerlist">
                         <h3>Follow us</h3>
                 </ul>
             </div>
             <div class="col-md 12">
-                <small>©︎Relaccum 2019 ・　KaitoSuzuki</small>
-                <h5>Terms of Service ・ Privacy Policy ・ Cookie Policy</h5>
+                <h5>©︎Relaccum 2019 ・　KaitoSuzuki</h5>
             </div>
         </div>
     </div>
