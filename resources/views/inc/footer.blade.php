21bdce61e6a6946b1e98dca12397ff62b68f1bfb
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-10 17:47:18 +0530

    　plan

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <h3>ABOUT</h3>
+                    <h3 class="footermenu">ABOUT</h3>
                     <ul id="footerlist">
                         <li class="footercontent">Relaccumとは</li>
                         <li class="footercontent">利用規約</li>
@@ -11,7 +11,7 @@
                     </ul>
                 </div>
                 <div class="col-md-4">
-                    <h3>Menu</h3>
+                    <h3 class="footermenu">Menu</h3>
                     <ul id="footerlist">
                         <li class="footercontent"><a href="">グループを探す</a></li>
                         <li class="footercontent"><a href="">イベントを探す</a></li>
@@ -19,7 +19,7 @@
                     </ul>
                 </div>
                 <div class="col-md-4">
-                    <h3>Follow us</h3>
+                    <h3 class="footermenu">Follow us</h3>
                     <ul id="footerlist">
                         <li id="snsicon" class="footercontent"><a href="https://www.facebook.com/profile.php?id=100043318232181"><i class="fab fa-facebook fa-2x"></i></a></li>
                         <li id="snsicon" class="footercontent"><a href="https://www.instagram.com/suzukai_kaito830/?hl=ja"><i class="fab fa-instagram fa-2x"></i></a></li>
