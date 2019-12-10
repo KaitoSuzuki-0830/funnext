86ee71659107f898f609cd7c6b802b7870ccba98
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-10 17:44:29 +0530

    　plan

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -3,27 +3,27 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
+                    <h3>ABOUT</h3>
                     <ul id="footerlist">
-                        <h3>ABOUT</h3>
                         <li class="footercontent">Relaccumとは</li>
                         <li class="footercontent">利用規約</li>
                         <li class="footercontent">お問い合わせ</li>
                     </ul>
                 </div>
                 <div class="col-md-4">
+                    <h3>Menu</h3>
                     <ul id="footerlist">
-                        <h3>Menu</h3>
                         <li class="footercontent"><a href="">グループを探す</a></li>
                         <li class="footercontent"><a href="">イベントを探す</a></li>
                         <li class="footercontent"><a href="">カテゴリーを探す</a></li>
                     </ul>
                 </div>
                 <div class="col-md-4">
+                    <h3>Follow us</h3>
                     <ul id="footerlist">
-                        <h3>Follow us</h3>
-                        <li id="snsicon" class="footercontent"><a href=""><i class="fab fa-facebook fa-2x"></i></a></li>
-                        <li id="snsicon" class="footercontent"><a href=""><i class="fab fa-instagram fa-2x"></i></a></li>
-                        <li id="snsicon" class="footercontent"><a href=""><i class="fab fa-twitter fa-2x"></i></a></li>
+                        <li id="snsicon" class="footercontent"><a href="https://www.facebook.com/profile.php?id=100043318232181"><i class="fab fa-facebook fa-2x"></i></a></li>
+                        <li id="snsicon" class="footercontent"><a href="https://www.instagram.com/suzukai_kaito830/?hl=ja"><i class="fab fa-instagram fa-2x"></i></a></li>
+                        <li id="snsicon" class="footercontent"><a href="https://twitter.com/Sakigake830"><i class="fab fa-twitter fa-2x"></i></a></li>
                     </ul>
                 </div>
             </div>
