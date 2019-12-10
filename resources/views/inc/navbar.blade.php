f991156c042b7b13456de5c1e42e82ab097ebcbe
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-10 21:12:00 +0530

    　footer

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Relaccum') }}
+            <img src="uploads/logo/relaccum.png" alt="relaccum">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
--- resources/views/outline/index.blade.php
+++ resources/views/outline/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 @section('content')
 <div class="input-group bg-dark text-white p-2">
-    {{-- Search box  --}}
     <div class="col-md-1"></div>
     <div class="col-md-10">
         <form action="/outlinesearch" method="get">
