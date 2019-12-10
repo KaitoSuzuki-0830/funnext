92e8a168e380c94759d44395db8e1cb2d62b535f
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-10 19:48:18 +0530

    　plan

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -20,9 +20,9 @@
                 <div class="col-md-4">
                     <h3 class="footermenu">Follow us</h3>
                     <ul id="footerlist">
-                        <li id="snsicon"><a href="https://www.facebook.com/profile.php?id=100043318232181"><i class="fab fa-facebook fa-2x"></i></a></li>
-                        <li id="snsicon"><a href="https://www.instagram.com/suzukai_kaito830/?hl=ja"><i class="fab fa-instagram fa-2x"></i></a></li>
-                        <li id="snsicon"><a href="https://twitter.com/Sakigake830"><i class="fab fa-twitter fa-2x"></i></a></li>
+                        <li id="snsicon" class="footercontent"><a href="https://www.facebook.com/profile.php?id=100043318232181"><i class="fab fa-facebook fa-2x"></i></a></li>
+                        <li id="snsicon" class="footercontent"><a href="https://www.instagram.com/suzukai_kaito830/?hl=ja"><i class="fab fa-instagram fa-2x"></i></a></li>
+                        <li id="snsicon" class="footercontent"><a href="https://twitter.com/Sakigake830"><i class="fab fa-twitter fa-2x"></i></a></li>
                     </ul>
                 </div>
             </div>
