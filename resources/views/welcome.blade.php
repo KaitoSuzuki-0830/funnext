f5ac6868f7935dabfab1e7f94f9bdeacdda4299c
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-11 10:05:07 +0530

    　first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,6 +64,7 @@
         </style>
     </head>
     <body>
+            <img src="https://images.unsplash.com/photo-1490578474895-699cd4e2cf59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80"  width="100%" id="firstimage">
         <img src="" alt="">
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
@@ -82,13 +83,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <img src="https://images.unsplash.com/photo-1490578474895-699cd4e2cf59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80" alt="" width="100%" id="firstimage">
                     <img src="uploads/logo/relaccumtitle.png" height="10%" width="30%" id="firsttitle">
-                    <h3 id="firstsubtitle">新しい関係がここにある</h3>
+                    <h3 id="firstsubtitle">さあ、新しいリレーションを探そう！</h3>
                 </div>
-
-
-
             </div>
         </div>
 
