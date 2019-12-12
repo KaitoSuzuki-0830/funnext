bb23a854d3d5fd67191fc843b3766f282d44e5cd
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-12 22:53:39 +0530

    　help

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -2,11 +2,23 @@
 @section('content')
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col-sm-8">
-                img
+        @foreach($groups as $group)
+            <div class="d-inline-block">
+            <div class="card-deck" style="width:24.5rem;">
+              <a href="{{route('groups.show',['id'=>$group->id])}}" class="card">
+              <img src="{{asset($group->featured)}}" class="card-img" alt="{{$group->name}}" height="200px" width="200px">
+              <div class="card-img-overlay">
+                  <div class="card-title"><h3>{{$group->name}}</h3></div>
+                  <p class="card-text"><i class="fas fa-users"></i> members</p>
+                  <p class="card-text"><i class="fas fa-tag"></i>{{$group->category->name}}</p>
+              </div>
+              </a>
             </div>
-        </div>
+            <div class="row justify-content-center">
+              <a href="{{route('group.join',['groupid'=>$group->id,'userid'=>$user->id])}}" class="btn btn-success btn-sm text-reset" role="button"><i class="fas fa-plus-circle fa-1.5x">Join</i></a>
+            </div>
+          </div>
+        @endforeach
     </div>
 </body>
 @endsection
