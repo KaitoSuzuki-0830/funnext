1cf87e3a312525873590f4e176deaddbf290c7d6
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-12 19:22:00 +0530

    　help

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid" id="about1">
-    <img src="uploads/logo/relaccumtitle.png" width="50%" height="30%">
-    <h3>リレキューむ</h3>
+    <div class="col-md-6">
+        <img src="uploads/logo/relaccumtitle.png" width="50%" height="30%">
+    </div>
+    <div class="col-md-4">
+        <h3>リレキューむ</h3>
+    </div>
 </div>
 @endsection
