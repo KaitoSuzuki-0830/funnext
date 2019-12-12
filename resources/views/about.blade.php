9a8db46bc3baa2a4562e9e5d6ae5c7802dcd79f3
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-12 18:46:19 +0530

    　help

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,18 +1,7 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header" id="themetitle">Relaccumとは</div>
-
-                <div class="card-body">
-
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container-fluid">
+    <img src="uploads/logo/relaccumtitle.png" width="100%" height="20%">
 </div>
 @endsection
 　
