b0449e16a68636d97710081ab4fe13ad0aed932a
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-12 11:24:55 +0530

    　outline

--- resources/views/plans/show.blade.php
+++ resources/views/plans/show.blade.php
@@ -9,7 +9,6 @@
 </div>
 
  <div class="card" style="width:100%;">
-        <img src="{{ $plan->featured }}" class="card-img-top" alt="{{$plan->title}}" height="200px" width="200px">
             <div class="card-body">
                 <div class="card-title"><h3>{{$plan->title}}</h3></div>
                 {{-- <p class="card-text"><i class="fas fa-map-marker-alt"></i> {{$plan->prefecture->name}}</p> --}}
