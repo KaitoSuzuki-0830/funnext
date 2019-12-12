2b975e993551d51f99ec31e19940a709ef753f26
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-13 00:01:37 +0530

    　search

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 @section('content')
+<div class="container-fluid" id="about0">
+</div>
 <div class="container-fluid" id="about1">
         <img src="uploads/logo/relaccumtitle.png" width="50%" height="30%">
         <h3>リレキューむ</h3>
