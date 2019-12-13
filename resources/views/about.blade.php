1362e30576438281805a3f3d0fab3dd964002e19
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-13 22:27:44 +0530

    　about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -41,18 +41,28 @@
 <div class="container-fluid" id="about3">
     <div class="container">
         <h1 class='abouttitle'>やりたいことを探す</h1>
-        <div class="option"><img src="uploads/logo/サーチアイコン.png" class="iconabout3"></div>
-        <div class="option2">
-            <h3>探す</h3>
-            <p><a href="">グループ</a></p>
-            <p><a href="">カテゴリー</a></p>
-            <p><a href="">イベント</a></p>
-        </div>
-        <div class="option"><img src="uploads/logo/プラスのアイコン素材.png" class="iconabout3"></div>
-        <div class="option2">
-            <h3>作る</h3>
-            <p><a href="">グループ</a></p>
-            <p><a href="">イベント</a></p>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="option"><img src="uploads/logo/サーチアイコン.png" width="50%" height="50%"></div>
+            </div>
+            <div class="col-md-4">
+                <div class="option">
+                    <h3>探す</h3>
+                    <p><a href="">グループ</a></p>
+                    <p><a href="">カテゴリー</a></p>
+                    <p><a href="">イベント</a></p>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="option"><img src="uploads/logo/プラスのアイコン素材.png" width="50%" height="50%"></div>
+            </div>
+            <div class="col-md-4">
+                <div class="option">
+                    <h3>作る</h3>
+                    <p><a href="">グループ</a></p>
+                    <p><a href="">イベント</a></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
