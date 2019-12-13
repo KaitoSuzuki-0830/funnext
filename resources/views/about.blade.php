fb8fcc52f977301eee2c9794774615f9255c1f8b
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-13 11:28:25 +0530

    　help

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid" id="about0">
-    <a href="">Relaccum</a>
-    <a href="">できること</a>
-    <a href="">探す</a>
-    <a href="">Follow Us</a>
+    <a href="" id="abouttitle">Relaccum</a>
+    <a href="" id="abouttitle">できること</a>
+    <a href="" id="abouttitle">探す</a>
+    <a href="" id="abouttitle">Follow Us</a>
 </div>
 <div class="container-fluid" id="about1">
         <img src="uploads/logo/relaccumtitle.png" width="50%" height="30%">
