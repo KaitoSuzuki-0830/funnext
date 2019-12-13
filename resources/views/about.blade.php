d621e974960ed4fa6699b870b17cabfc28e7bcdd
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-13 11:24:06 +0530

    　help

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid" id="about0">
+    <a href="">Relaccum</a>
+    <a href="">できること</a>
+    <a href="">探す</a>
+    <a href="">Follow Us</a>
 </div>
 <div class="container-fluid" id="about1">
         <img src="uploads/logo/relaccumtitle.png" width="50%" height="30%">
-        <h3>リレキューむ</h3>
+        <h3></h3>
 </div>
 @endsection
