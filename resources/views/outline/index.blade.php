6cef85d15dac597a700ce8e526726fe667286552
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-14 16:43:05 +0530

    　about

--- resources/views/outline/index.blade.php
+++ resources/views/outline/index.blade.php
@@ -79,7 +79,7 @@
     <div class="card text-center" style="width: 22rem;">
         <div class="card-body">
           <h5 class="card-title">{{ $plan->title}}</h5>
-          <p class="card-text"><i class="fas fa-map-marker-alt"></i> {{ $plan->place }}</p>
+          <p class="card-text" id="eventplace"><i class="fas fa-map-marker-alt"></i> {{ $plan->place }}</p>
           <a href="{{route('plans.show',['id'=>$plan->id])}}" class="btn btn-primary">詳細へ</a>
         </div>
     </div>
