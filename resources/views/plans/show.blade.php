35328bacb2c3beee5e47f720bf3327030da75689
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-15 19:57:54 +0530

    　about

--- resources/views/plans/show.blade.php
+++ resources/views/plans/show.blade.php
@@ -1,20 +1,19 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-<div class="row">
-    <div class="col-md-6"><h2>イベント　詳細</h2></div>
-    <div class="col-md-4"></div>
-    <div class="col-md-1"><iframe src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Flaravel-myproduct.herokuapp.com%2Fplans%2F11&layout=button&size=large&appId=467764400530816&width=79&height=28" width="79" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></<iframe></div>
-    <div class="col-md-1"><a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="私はこのイベントに参加します！参加者、募集なう！！！" data-hashtags="relaccum #リレキューム　#Relaccum #イベント" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></<a></div>
+<<a class="row">
+    <div class="col-md-12" id="pagetitle"><h2>イベント詳細</h2></div>
 </div>
 <div class="row">
     <div class="col-md-6">
         <div class="card">
             <div class="card-body">
                 <div class="card-title"><h3>{{$plan->title}}</h3></div>
-                    <p class="card-text"><i class="fas fa-map-marker-alt"></i>　{{$plan->place}}</p>
+                    <p class="card-text"><i class="fas fa-map-marker-alt"></i>{{$plan->place}}</p>
                     <p class="card-text"><i class="fas fa-tag"></i> {{$plan->category->name}}</p>
                     <p class="card-text">{{$plan->description}}</p>
+                    <iframe src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Flaravel-myproduct.herokuapp.com%2Fplans%2F11&layout=button&size=large&appId=467764400530816&width=79&height=28" width="79" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="私はこのイベントに参加します！参加者、募集なう！！！" data-hashtags="relaccum #リレキューム　#Relaccum #イベント" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></<a>
                     <p class="card-text"><i class="fas fa-yen-sign"></i> {{$plan->price}}
                         <form action="{{ route('pay') }}" method="POST">
                         {{ csrf_field() }}
@@ -35,7 +34,6 @@
         </div>
     </div>
     <div class="col-md-6">
-            <a href="https://www.google.com/maps/dir/?api=1&destination={{ $plan->place }}">Google map で開く</a>
                 <div id="map_info">
                   <p id="venue">会場先</p>
                   <p id="address">{{ $plan->place }}</p>
@@ -122,6 +120,7 @@
                 </script>
                 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9iATwHWcm3sUqBSZnJB3v1VMbV9_s6lw&callback=initMap" async defer></script>
                 <!-- YOUR_API_KEYの部分は取得した APIキーで置き換えます。 -->
+                <a href="https://www.google.com/maps/dir/?api=1&destination={{ $plan->place }}">Google mapで開く</a>
     </div>
 </div>
 <hr>
