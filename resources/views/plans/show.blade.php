0a4556020a96202e5cdf64a47891e73822b379c1
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-15 18:23:23 +0530

    　about

--- resources/views/plans/show.blade.php
+++ resources/views/plans/show.blade.php
@@ -34,7 +34,24 @@
             </div>
         </div>
     </div>
-    <div class="col-md-6"></div>
+    <div class="col-md-6">
+        <div id="map" style="height: 500px; width: 80%; margin: 2rem auto 0;"></div>
+            <!-- jqueryの読み込む -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <!-- google map api -->
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9iATwHWcm3sUqBSZnJB3v1VMbV9_s6lw"
+            ></script>
+            <!-- js -->
+            <script type="text/javascript">
+              var map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                  lat: -34.397, //緯度を設定
+                  lng: 150.644 //経度を設定
+                },
+                zoom: 8 //地図のズームを設定
+              });
+            </script>
+    </div>
 </div>
 <hr>
 <div class="row">
