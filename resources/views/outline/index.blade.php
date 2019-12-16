5fb5a9f2dff4eaf1909badadbb9f3d3609e484f4
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-16 21:11:11 +0530

    　plans

--- resources/views/outline/index.blade.php
+++ resources/views/outline/index.blade.php
@@ -21,9 +21,9 @@
 <div class="container">
         <h2>イベント</h2>
         <div class="row">
-            <div class="col-md-2"><h5>もうすぐ開催のイベントをチェック</h5></div>
-            <div class="col-md-8"></div>
-            <div class="col-md-2">
+            <div class="col-md-5"><h5>もうすぐ開催のイベントをチェック</h5></div>
+            <div class="col-md-6"></div>
+            <div class="col-md-1">
                 <a href="{{route('plans.index')}}"><h5>すべて見る</h5></a>
             </div>
         </div>
@@ -43,9 +43,9 @@
 <div class="container">
     <h2>カテゴリー</h2>
         <div class="row">
-            <div class="col-md-2"><h5>興味のあるトピックからイベントを検索</h5></div>
-            <div class="col-md-8"></div>
-            <div class="col-md-2">
+            <div class="col-md-5"><h5>興味のあるトピックからイベントを検索</h5></div>
+            <div class="col-md-6"></div>
+            <div class="col-md-1">
                 <a href="{{route('category.index')}}"><h5>すべて見る</h5></a>
             </div>
         </div>
@@ -64,11 +64,11 @@
 <div class="container" id="groupposition">
     <h2>グループ</h2>
     <div class="row">
-        <div class="col-md-2">
+        <div class="col-md-5">
             <h5>あなたが惹かれるグループを見つけよう</h5>
         </div>
-        <div class="col-md-8"></div>
-        <div class="col-md-2">
+        <div class="col-md-6"></div>
+        <div class="col-md-1">
             <a href="{{route('groups.index')}}"><h5>すべて見る</h5></a>
         </div>
     </div>
