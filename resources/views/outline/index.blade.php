80d8e0ec21adb74071d4b4f8bff975b8974ea811
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-16 18:09:09 +0530

    　about

--- resources/views/outline/index.blade.php
+++ resources/views/outline/index.blade.php
@@ -53,7 +53,7 @@
                 <a href="{{route('category.index')}}"><h5>すべて見る</h5></a>
             </div>
         </div>
-@foreach($categories as $category)
+{{-- @foreach($categories as $category)
     <div class="d-inline-block">
         <div class="card-deck" style="width:18rem;">
             <a href="{{route('category.show',['id'=>$category->id])}}" class="card">
@@ -62,7 +62,7 @@
         </div>
         <h5 class="row justify-content-center">{{$category->name}}</h5>
     </div>
-@endforeach
+@endforeach --}}
 </div>
 <div class="container">
     <h2>イベント</h2>
