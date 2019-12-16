a9ac40ad1fc7f67202de315fb12abddf7b14df77
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-16 23:55:16 +0530

    　plans

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,7 +10,7 @@
 <div class="container-fluid" id="about1">
         <img src="uploads/logo/aboutsoccer.jpg" id="welcomeimage">
         <h2 class="aboutsubtitle1">世界が変わってサッカーはライブ配信でより身近で見ることが簡単になった。だから毎日部屋で応援している日々。</h2>
-        <h2 class="aboutsubtitle2">もし、この情熱と感動を分かち合える仲間がいれば、僕はもっとサッカーにハマるに違いない。</h2>
+        <h2 class="aboutsubtitle2">もし、この情熱と感動を分かち合える仲間と観れたら、僕はもっとサッカーにハマるに違いない。</h2>
 </div>
 <div class="container-fluid" id="about2">
     <div class="container">
