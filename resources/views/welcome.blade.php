20c7131dc89a0b60df1e1b56297c1dd42139bf7e
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-17 01:19:01 +0530

    　footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,12 +65,9 @@
         </style>
     </head>
     <body>
-        <div class="container-fluid" id="about1">
-            <img src="uploads/logo/aboutsoccer.jpg" id="welcomeimage">
             <h2 class="aboutsubtitle1">世界が変わってサッカーはライブ配信でより身近で見ることが簡単になった。</h2>
             <h2 class="aboutsubtitle2">だから週末は部屋で応援している。だけど何かが足りない。</h2>
             <h2 class="aboutsubtitle3">もし、この情熱と感動を分かち合える仲間と観れたら、</h2>
             <h2 class="aboutsubtitle4">僕はもっとサッカーにハマるに違いない。</h2>
-        </div>
     </body>
 </html>
