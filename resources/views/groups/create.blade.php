761ffe036247ca262851317a71d16d0f890dcfc2
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-17 09:40:59 +0530

    　welcome

--- resources/views/groups/create.blade.php
+++ resources/views/groups/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container-fluid" id="groupimagebox">
     <img src="uploads/logo/groupsoccer.jpeg" id="groupimage">
-    <h1 class="servicedescription1">関係が続くためにグループを作ってみよう！</h1>
+    <h1 class="grouptext">関係が続くためにグループを作ってみよう！</h1>
 </div>
 <div class="container">
  <div class="card">
