c3b14f47748dd4fbdf1ee2b88dcdf5928790a6a4
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-18 00:14:11 +0530

    　about

--- resources/views/contact/confirm.blade.php
+++ resources/views/contact/confirm.blade.php
@@ -8,7 +8,7 @@
     <form method="POST" action="{{ route('contact.send') }}">
         @csrf
         <div class="form-group">
-        <label>メールアドレス</label>
+        <label>メールアドレス:</label>
             {{ $inputs['email'] }}
         <input
             name="email"
@@ -17,7 +17,7 @@
             class="form-control">
         </div>
         <div class="form-group">
-        <label>タイトル</label>
+        <label>タイトル:</label>
             {{ $inputs['title'] }}
         <input
             name="title"
@@ -26,7 +26,7 @@
             class="form-control">
         </div>
         <div class="form-group">
-        <label>お問い合わせ内容</label>
+        <label>お問い合わせ内容:</label>
             {!! nl2br(e($inputs['body'])) !!}
         <input
             name="body"
@@ -35,15 +35,18 @@
             class="form-control">
         </div>
         <div class="row">
+        <div class="col-md-3 offset-3"></div>
         <div class="form-group">
         <button type="submit" name="action" value="back" class="btn btn-secondary">
             <i class="fas fa-long-arrow-alt-left"></i> 修正する
         </button>
         </div>
+        <div class="col-md-3">
         <div class="form-group">
         <button type="submit" name="action" value="submit" class="btn btn-primary">
             <i class="far fa-paper-plane"></i> 送信する
         </button>
+        </div>
         </div>
     </div>
     </form>
