47df576ad35af68fb43a5a04b5628b60e5d17b3a
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-17 08:42:15 +0530

    　welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,10 +64,10 @@
         </style>
     </head>
     <body id="welcomeimage">
-        <h2>ここで、その情熱と感動を分かち会おう。</h2>
+        <h2 class="aboutsubtitle1">ここで、その情熱と感動を分かち会おう。</h2>
         <div class="end">
             @include('inc.footer')
         </div>
-        {{-- class="aboutsubtitle1" class="aboutsubtitle2" class="aboutsubtitle3" class="aboutsubtitle4" --}}
+        {{--  class="aboutsubtitle2" class="aboutsubtitle3" class="aboutsubtitle4" --}}
     </body>
 </html>
