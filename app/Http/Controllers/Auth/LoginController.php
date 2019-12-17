88f0177c523f9d7d004b898da1fbeebfcfa02cf6
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-18 00:23:06 +0530

    　about

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/welcome';
+    protected $redirectTo = 'outline.index';
 
     /**
      * Create a new controller instance.
