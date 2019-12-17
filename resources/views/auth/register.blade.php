ba7fd5635bb87780c9ac7bc287109a7d985f7025
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-17 23:35:00 +0530

    　about

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -68,7 +68,7 @@
                                 </button>
                             </div>
                             <div class="col-md-2">
-                                <a href="/login"class="text-right"><i class="fas fa-sign-in-alt"></i>ログインはこちら</a>
+                                <a href="/login"class="text-right"><i class="fas fa-sign-in-alt"></i> ログイン</a>
                             </div>
                         </div>
                         <hr>
