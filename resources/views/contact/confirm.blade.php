ceec662a40f80cd2b99fa921d79750eeae64edfc
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-17 23:53:00 +0530

    　about

--- resources/views/contact/confirm.blade.php
+++ resources/views/contact/confirm.blade.php
@@ -1,36 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('contact.send') }}">
-    @csrf
-
-    <label>メールアドレス</label>
-    {{ $inputs['email'] }}
-    <input
-        name="email"
-        value="{{ $inputs['email'] }}"
-        type="hidden">
-
-    <label>タイトル</label>
-    {{ $inputs['title'] }}
-    <input
-        name="title"
-        value="{{ $inputs['title'] }}"
-        type="hidden">
-
-
-    <label>お問い合わせ内容</label>
-    {!! nl2br(e($inputs['body'])) !!}
-    <input
-        name="body"
-        value="{{ $inputs['body'] }}"
-        type="hidden">
-
-    <button type="submit" name="action" value="back">
-        入力内容修正
-    </button>
-    <button type="submit" name="action" value="submit">
-        送信する
-    </button>
-</form>
+<div class="card-header">お問い合わせ(確認）</div>
+<div class="card-body">
+    <form method="POST" action="{{ route('contact.send') }}">
+        @csrf
+        <div class="form-group">
+        <label>メールアドレス</label>
+            {{ $inputs['email'] }}
+        <input
+            name="email"
+            value="{{ $inputs['email'] }}"
+            type="hidden">
+        </div>
+        <div class="form-group">
+        <label>タイトル</label>
+            {{ $inputs['title'] }}
+        <input
+            name="title"
+            value="{{ $inputs['title'] }}"
+            type="hidden">
+        </div>
+        <div class="form-group">
+        <label>お問い合わせ内容</label>
+            {!! nl2br(e($inputs['body'])) !!}
+        <input
+            name="body"
+            value="{{ $inputs['body'] }}"
+            type="hidden">
+        </div>
+        <div class="form-group">
+        <button type="submit" name="action" value="back">
+            入力内容修正
+        </button>
+        </div>
+        <div class="form-group">
+        <button type="submit" name="action" value="submit">
+         送信する
+        </button>
+        </div>
+    </form>
+</div>
 @endsection
