dc02cd62d6f85a0dc7b2f0e2c20e2ea6d13e04fd
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-17 16:04:23 +0530

    　welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Kaushan+Script&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
