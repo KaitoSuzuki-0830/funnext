aa1d25a8607dac72bc3d6a49961e39676e20bd64
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-18 00:16:39 +0530

    　about

--- resources/views/contact/confirm.blade.php
+++ resources/views/contact/confirm.blade.php
@@ -35,13 +35,13 @@
             class="form-control">
         </div>
         <div class="row">
-        <div class="col-md-3 offset-3"></div>
+        <div class="col-md-4 offset-4"></div>
         <div class="form-group">
         <button type="submit" name="action" value="back" class="btn btn-secondary">
             <i class="fas fa-long-arrow-alt-left"></i> 修正する
         </button>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-2">
         <div class="form-group">
         <button type="submit" name="action" value="submit" class="btn btn-primary">
             <i class="far fa-paper-plane"></i> 送信する
