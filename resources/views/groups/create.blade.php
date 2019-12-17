c00e379f90d61ea328405d2d777a6302c3055af2
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-17 09:50:36 +0530

    　welcome

--- resources/views/groups/create.blade.php
+++ resources/views/groups/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid" id="groupimagebox">
-    <img src="uploads/logo/groupcreate.jpeg" id="groupimage">
+    <img src="uploads/logo/groupsoccer.jpeg" id="groupimage">
     <h1 class="grouptext">関係が続くためにグループを作ってみよう！</h1>
 </div>
 <div class="container">
