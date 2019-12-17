a89e1b2a91a2f52bbaf0e782e9f3ad1c3356d695
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-18 00:19:57 +0530

    　about

--- resources/views/contact/thanks.blade.php
+++ resources/views/contact/thanks.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container"　id="shortcontent8">
 <h1>{{ __('送信完了') }}</h1>
 <p>お問い合わせありがとうございました。すぐにご対応させていただきます。</p>
 </div>
