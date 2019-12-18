0af596bc492078c877a284f64471f7ad66f4692b
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-18 18:59:19 +0530

    　welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,34 +79,13 @@
         </div>
 
         <script>
-            $(function(){
-                var effect_pos = 300; // 画面下からどの位置でフェードさせるか(px)
-                var effect_move = 50; // どのぐらい要素を動かすか(px)
-                var effect_time = 800; // エフェクトの時間(ms) 1秒なら1000
-
-                // フェードする前のcssを定義
-                $('#welcomebar').css({
-                    opacity: 0,
-                    transform: 'translateY('+ effect_move +'px)',
-                    transition: effect_time + 'ms'
-                });
-
-                // スクロールまたはロードするたびに実行
-                $(window).on('scroll load', function(){
-                    var scroll_top = $(this).scrollTop();
-                    var scroll_btm = scroll_top + $(this).height();
-                    effect_pos = scroll_btm - effect_pos;
-
-                    // effect_posがthis_posを超えたとき、エフェクトが発動
-                    $('#welcomebar').each( function() {
-                        var this_pos = $(this).offset().top;
-                        if ( effect_pos > this_pos ) {
-                            $(this).css({
-                                opacity: 1,
-                                transform: 'translateY(0)'
-                            });
-                        }
-                    });
+            jQuery(function($){
+                $(window).on('load scroll', function(){
+                    if ($(window).scrollTop() > 300) {
+                    $('#welcomebar').fadeIn(400);
+                    } else {
+                    $('#welcomebar').fadeOut(400);
+                    }
                 });
             });
 
@@ -122,7 +101,7 @@
                     return false;
                   });
                 });
-            </script>
+        </script>
         <div class="container-fluid" id="welcomepage">
             <img src="uploads/logo/titleimage.jpg" id="titleimage"　height="600" width="100%">
             <a href="{{route('outline.index')}}" id="logoposition"><h2 id="servicename">FunNext</h2></a>
