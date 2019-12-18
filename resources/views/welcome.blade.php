dc590f933b23055dae7b99c938a30aa8c1749484
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-18 19:17:43 +0530

    　welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,11 +89,11 @@
                 });
             });
 
-            $(function(){
+            jQuery(function($){
                   // #で始まるリンクをクリックしたら実行されます
                   $('a[href^="#"]').click(function() {
                     // スクロールの速度
-                    var speed = 1500; // ミリ秒で記述
+                    var speed = 2000; // ミリ秒で記述
                     var href= $(this).attr("href");
                     var target = $(href == "#" || href == "" ? 'html' : href);
                     var position = target.offset().top;
