5c698caa0e42fe9b87391d8fbc2cc163afe2c3f4
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-19 16:26:09 +0530

    　outline

--- resources/views/plans/show.blade.php
+++ resources/views/plans/show.blade.php
@@ -6,7 +6,7 @@
     <h2 id="pagetitle">{{$plan->title}}</h2>
     <div class="row">
         <div class="col-md-1">アバター</div>
-        <div class="col-md-1">{{$organiser->name}}</div>
+        <div class="col-md-1">名前</div>
         <div class="col-md-8"></div>
         <div class="col-md-1"><iframe src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Flaravel-myproduct.herokuapp.com%2Fplans%2F11&layout=button&size=large&appId=467764400530816&width=79&height=28" width="79" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
         <div class="col-md-1"><a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="私はこのイベントに参加します！参加者、募集なう！！！" data-hashtags="relaccum #リレキューム　#Relaccum #イベント" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></<a></div>
