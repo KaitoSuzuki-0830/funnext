1ef1b4a0d9e1a0a18e8044aef1a5d8b2591bfbc6
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-19 15:15:38 +0530

    　outline

--- resources/views/outline/index.blade.php
+++ resources/views/outline/index.blade.php
@@ -68,7 +68,7 @@
             </div>
         </div>
     @foreach($categories as $category)
-    <div class="d-inline-block">
+    <div class="d-inline-block" id="onecard">
         <div class="card-deck" style="width:18rem;">
             <a href="{{route('category.show',['id'=>$category->id])}}" class="card" id="cardsize">
                 <img src="{{ $category->featured}}" alt="{{ $category->name}}" class="card-img" height="150px" width="150px">
