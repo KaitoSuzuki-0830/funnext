3d4f079fc81c063f161e9698ec87cf5e2de0df3c
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-19 20:40:14 +0530

    　outline

--- resources/views/plans/show.blade.php
+++ resources/views/plans/show.blade.php
@@ -15,7 +15,7 @@
 </div>
 <div class="container-fluid" id="planmain">
 <div class="container">
-    <img src="{{asset($plan->featured)}}" alt="{{$plan->name}}" height="500px" width="100%">
+    <img src="{{asset($plan->featured)}}"  alt="{{$plan->title}}" height="200px" width="200px">
     <div class="row">
         <div class="col-md-6">
             <div class="card" id="eventimage">
