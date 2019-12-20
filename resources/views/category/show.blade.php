257772cfb6da17fd50cb12343d0c74c6e3eecbd9
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-12-20 11:32:18 +0530

    　single

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -2,19 +2,32 @@
 @section('content')
 <body>
     <div class="container">
-        @foreach($category->groups as $group)
-            <div class="d-inline-block">
-            <div class="card-deck" style="width:24.5rem;">
-              <a href="{{route('groups.show',['id'=>$group->id])}}" class="card">
-              <img src="{{asset($group->featured)}}" class="card-img" alt="{{$group->name}}" height="200px" width="200px">
-              <div class="card-img-overlay">
-                  <div class="card-title"><h3>{{$group->name}}</h3></div>
-                  <p class="card-text"><i class="fas fa-users"></i> members</p>
-                  <p class="card-text"><i class="fas fa-tag"></i>{{$group->category->name}}</p>
-              </div>
-              </a>
-            </div>
-          </div>
+        @foreach ($plans as $plan)
+            <a href="{{ route('plans.show',['id'=>$plan->id])}}" id="planoverall">
+                <div class="d-inline-block" id="cardsize">
+                    <div class="card" style="width: 18rem;" id="outlineeventcard">
+                        <img src="{{asset($plan->featured)}}" class="card-img-top" alt="{{$plan->title}}">
+                        <div class="card-body" id="outlinecard">
+                        <h5 class="card-title" id="eventtitle">{{ $plan->title}}</h5>
+                        <table class="eventtableposition">
+                            <tr class="tablelist">
+                                <td><i class="far fa-clock"></i></td>
+                                <td>{{$plan->date}}</td>
+                            </tr>
+                            <tr class="tablelist">
+                                <td><i class="fas fa-users"></i></td>
+                                <td>5</td>
+                            </tr>
+                            <tr class="tablelist">
+                                <td><i class="fas fa-hand-holding-usd"></i></td>
+                                <td>¥{{$plan->price}}</td>
+                            </tr>
+                        </table>
+                        <a href="{{ route('plans.show',['id'=>$plan->id])}}" class="btn btn-info"　role="button" id="showbutton">Details</a>
+                        </div>
+                    </div>
+                </div>
+            </a>
         @endforeach
     </div>
 </body>
