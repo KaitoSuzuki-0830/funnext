cda680bf5f555920901a04bed12ca7da18fb3731
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2020-02-04 16:12:29 +0900

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,12 +117,12 @@
                         <img src="uploads/photo/audience.jpg" id="subwelcomeimage" height="50%" width="100%">
                     </div>
                     <div class="col-md-6">
-                        <p class="welcomedescription">誰でもカンタンにイベントに参加しサッカーが好きな人との新しい出会いがあります。</p>
-                        <p class="welcomedescription">また、あなた自身で試合観戦などのイベントを企画することもできます。</p>
-                        <p class="welcomedescription">リアルで集まって大好きなサッカーを一緒に楽しめることができるサービスです。</p>
-                        <h2 id="welcomephrase">さあ、楽しみましょう。</h2>
+                        <p class="welcomedescription">誰でもカンタンにイベントに参加可能なサッカー専門イベントサイト。</p>
+                        <p class="welcomedescription">また、あなた自身で試合観戦などのイベントを企画することも可能。</p>
+                        <p class="welcomedescription">リアルで集まって大好きなサッカーを一緒に楽しめることができるサービス。</p>
                     </div>
                 </div>
+                <h2 id="welcomephrase">さあ、楽しみましょう。</h2>
             </div>
         </div>
         <div class="container-fluid" id="welcomecon2">
