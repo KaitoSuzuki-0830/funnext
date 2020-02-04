528b7de89b18d691ff2bb2cf29f38b86208530d6
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2020-02-04 18:28:09 +0900

    welcome

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,14 +61,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-4 offset-md-4">
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('登録') }}
                                 </button>
-                            </div>
-                            <div class="col-md-2">
-                                <a href="/login"class="text-right"><i class="fas fa-sign-in-alt"></i> ログイン</a>
                             </div>
                         </div>
                         <hr>
