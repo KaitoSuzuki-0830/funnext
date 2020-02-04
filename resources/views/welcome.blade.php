195d3ed6c1c6dd6cb8f17c5b71e39b6695de3ce7
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2020-02-04 15:54:54 +0900

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,13 +113,13 @@
             <div class="container">
                 <h2 id="welcome1">FunNext</h2>
                 <div class="col-md-6">
-                    <img src="uploads/photo/titleimage.jpg" id="titleimage" height="100%" width="50%">
+                    <img src="uploads/photo/titleimage.jpg" id="titleimage" height="100%" width="100%">
                 </div>
                 <div class="col-md-6">
-                <p class="welcomedescription">誰でもカンタンにイベントに参加しサッカーが好きな人との新しい出会いがあります。</p>
-                <p class="welcomedescription">また、あなた自身で試合観戦などのイベントを企画することもできます。</p>
-                <p class="welcomedescription">リアルで集まって大好きなサッカーを一緒に楽しめることができるサービスです。</p>
-                <h2 id="welcomephrase">さあ、楽しみましょう。</h2>
+                    <p class="welcomedescription">誰でもカンタンにイベントに参加しサッカーが好きな人との新しい出会いがあります。</p>
+                    <p class="welcomedescription">また、あなた自身で試合観戦などのイベントを企画することもできます。</p>
+                    <p class="welcomedescription">リアルで集まって大好きなサッカーを一緒に楽しめることができるサービスです。</p>
+                    <h2 id="welcomephrase">さあ、楽しみましょう。</h2>
                 </div>
             </div>
         </div>
